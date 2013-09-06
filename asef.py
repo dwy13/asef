3740a553ad8b541fa6393dbd84196aa45af82de1
yuyf10 <yuyf10@gmail.com>
2013-09-06 23:59:16 +0800

    Update asef.py

--- asef.py
+++ asef.py
@@ -8,19 +8,29 @@ import subprocess
 
 apkurl = sys.argv[1]
 
+device = " -e "
+
 #set tools dir
 adb = "/home/yu/android-sdks/platform-tools/adb"
 aapt = "/home/yu/android-sdks/build-tools/18.0.1/aapt"
+
+#start adb
+os.system(adb+" start-server")
+if len(sys.argv) == 3 :
+	device = " -s "+sys.argv[2]+":5555"
+	os.system(adb+" connect "+sys.argv[2])
 
 #read apkinfo
 apkinfo = os.popen(aapt+" d badging "+ apkurl)
 apkinfo = apkinfo.read()
 
+print "read apkinfo"
 #get apkname
 start = apkinfo.find("package")
 end = apkinfo.find("versionCode",start)
 apkpackage = apkinfo[start:end-1]
 apkname = apkpackage[apkpackage.find("'")+1:apkpackage.rfind("'")]
+
 
 #get launch activity
 start = apkinfo.find("launchable-activity")
@@ -28,8 +38,7 @@ end = apkinfo.find("label",start)
 apkactivity = apkinfo[start:end-1]
 apkactivity = apkactivity[apkactivity.find("'")+1:apkactivity.rfind("'")]
 
-print apkname
-print apkactivity
+
 '''
 if os.path.exists("results") == False:
 	os.mkdir("results")
@@ -43,16 +52,25 @@ os.mkdir("results/"+apkname+testdir)
 netdir = "results/"+apkname+testdir+"/network_traffic.pcap"
 '''
 
+os.system(adb+device+" shell input keyevent 82")
+
+print "install apk"
+
 #install apk
-os.system(adb+" -e install "+apkurl)
+os.system(adb+device+" install "+apkurl)
 
+print "launch apk"
 #launch apk
-os.system(adb+" -e shell am start -W "+apkname+"/"+apkactivity)
+os.system(adb+device+" shell am start -W "+apkname+"/"+apkactivity)
 
-time.sleep(10)
+time.sleep(30)
 
+print "begin sending gestures"
 #send gestures
-os.system(adb+" -e shell monkey -p "+apkname+" 100")
+print time.ctime()
+os.system(adb+device+" shell monkey -p "+apkname+" 1000")
+print time.ctime()
 
+print "unistall apk"
 #unistall apk
-os.system(adb+" -e uninstall "+apkname)
+os.system(adb+device+" uninstall "+apkname)
